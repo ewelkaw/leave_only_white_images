803e5a873bda402c3b6c2594a7f3646354f2162d
Ewelina Szmielew <ewelina@szmielew.pl>
2020-10-16 22:37:16 +0200

    Add delete non slide files option

--- README.md
+++ README.md
@@ -1,7 +1,16 @@
-1. Running:
+1. **Running:**
 ```bash
-python3 check_images.py images/
+python3 check_images.py images/ delete
 ```
+python3 check_images.py path_to_images option_if_we_want_to_delete_non_slides
 
-2. CV2 installation:
-`https://pypi.org/project/opencv-python/`
+
+2. **CV2 installation:**
+
+`pip install opencv-python`
+
+From: `https://pypi.org/project/opencv-python/`
+
+3. **Pixels threshold**
+
+`pixels_thershold` is set to 50% (0.5) and it was set arbitrarily, so it can be changed anytime. If we want to check if the entire image is empty we need to set it to 100% (1.0).
--- check_images.py
+++ check_images.py
@@ -3,27 +3,36 @@ import cv2
 import sys
 
 
-def find_slide(img):
+def check_if_slide(img):
     height, width, _ = img.shape
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     thresh = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
 
+    # count non-zero (white) array elements
+    # and having pixels_thershold set arbitrarily to 50%
+    pixels_thershold = 0.5
     pixels = cv2.countNonZero(thresh)
-    result = True if pixels >= (height * width * 0.5) else False
+    result = True if pixels >= (height * width * pixels_thershold) else False
     return result
 
 
 if __name__ == "__main__":
     dir_name = sys.argv[1]
+    if_delete = False
+    if len(sys.argv) == 3:
+        if sys.argv[2] == "delete":
+            if_delete = True
 
     if os.path.exists(dir_name):
         for file in os.listdir(dir_name):
             if file.endswith(".png") or file.endswith(".jpg") or file.endswith(".jpeg"):
                 img_path = os.path.join(dir_name, file)
                 img = cv2.imread(img_path)
-                result = find_slide(img)
+                result = check_if_slide(img)
                 print(
-                    "Image {} is slide".format(file)
+                    "Image {} is slide".format(img_path)
                     if result is True
-                    else "Image {} isn't slide".format(file)
+                    else "Image {} isn't slide".format(img_path)
                 )
+                if if_delete and not result:
+                    os.remove(img_path)
